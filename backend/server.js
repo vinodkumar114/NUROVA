595bdf3bd545e8c57e8a6b98f39f79e1c28070f0
vinodkumar114 <vinokumar.chakali97@gmail.com>
2026-08-09 23:53:31 +0530

    Initial MERN project structure

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Backend is working!"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
